fcd698b14611b7bb45ec1353853ec8663c9f10fe
olibarklinuxlaptop <oliverbarkham@icloud.com>
2026-05-12 14:38:47 +0100

    Refactor RSA encryption methods.

--- src/mahkrabdtn/crypto/rsa.py
+++ src/mahkrabdtn/crypto/rsa.py
@@ -1,49 +1,59 @@
 from pathlib import Path
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
+from dataclasses import dataclass
 
+from mahkrabdtn.protocol.encryption import EncryptionMetadata
 from mahkrabdtn.protocol.parsing.text import parse_text
 
 
+@dataclass(slots=True)
 class NodeKeyPair:
     privateKeyPem: str
     publicKeyPem: str
-
-
-
-def create_node_key_pair(IDPath: str | Path) -> Path:
-    privateKeyPath = Path(IDPath).with_suffix(".privkey.pem")
-    privateKeyPath.parent.mkdir(parents=True, exist_ok=True)
     
-    if privateKeyPath.exists():
-        privateKeyPem = privateKeyPath.read_text(encoding="utf-8")
-    else:
-        privateKey = rsa.generate_private_key(
-            public_expononet=65537,
-            key_size=2048,
-        )
-        privateKeyPem = privateKey.private_bytes(
+
+class RsaEncryption:
+    def load_private_key(privateKeyPem: str) -> RSAPrivateKey:
+        privateKeyPem = parse_text(privateKeyPem, "privateKeyPem")
+        key = serialization.load_pem_private_key(privateKeyPem.encode("utf-8"))
+        
+        if not isinstance(key, RSAPrivateKey):
+            raise TypeError("private key must be of type RSA private key")
+        return key
+    
+    def serialize_public_key(publicKey: RSAPublicKey) -> str:
+        return publicKey.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        ).decode("utf-8")
+
+    def serialize_private_key(privateKey: RSAPrivateKey) -> str:
+        return privateKey.private_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PrivateFormat.PKCS8,
             encryption_algorithm=serialization.NoEncryption(),
         ).decode("utf-8")
         
-        privateKeyPath.write_text(privateKeyPem, encoding="utf-8")
-    
-    normalizedPrivateKeyPem = parse_text(privateKeyPem, "privateKeyPem")
-    publicKey = serialization.load_pem_private_key(normalizedPrivateKeyPem.encode("utf-8"))
-    
-    if not isinstance(publicKey, rsa.RSAPrivateKey):
-        raise TypeError("private key myst be of type RSA private key")
-    
-    publicKeyPem = publicKey.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo,
-    ).decode("utf-8")
-    normalizedPublicKeyPem = parse_text(publicKeyPem, "publicKeyPem")
-    
-    
-    return NodeKeyPair(
-        privateKeyPem=normalizedPrivateKeyPem,
-        publicKeyPem=normalizedPublicKeyPem
-    ),
+    def create_node_key_pair(IDPath: str | Path) -> Path:
+        privateKeyPath = Path(IDPath).with_suffix(".privkey.pem")
+        privateKeyPath.parent.mkdir(parents=True, exist_ok=True)
+        
+        if privateKeyPath.exists():
+            privateKeyPem = privateKeyPath.read_text(encoding="utf-8")
+        else:
+            privateKey = rsa.generate_private_key(
+                public_exponent=65537,
+                key_size=2048,
+            )
+            privateKeyPem = RsaEncryption.serialize_private_key(privateKey)
+            privateKeyPath.write_text(privateKeyPem, encoding="utf-8")
+        
+        publicKeyPem = RsaEncryption.serialize_public_key(RsaEncryption.load_private_key(privateKeyPem).public_key())
+        
+        return NodeKeyPair(
+            privateKeyPem=privateKeyPem,
+            publicKeyPem=publicKeyPem,
+        ),
+        
